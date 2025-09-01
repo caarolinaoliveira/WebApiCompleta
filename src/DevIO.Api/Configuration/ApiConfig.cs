687405a6718c50b2f35a6c9e3cee2abc7f8d46fb
Carolina Oliveira <carolina.oliveira@brf.com>
2025-09-01 09:20:39 -0300

    Remove bin and obj folders from repository

--- src/DevIO.Api/Configuration/ApiConfig.cs
+++ src/DevIO.Api/Configuration/ApiConfig.cs
@@ -1,19 +1,46 @@
-using AutoMapper;
-using DevIO.Api.ViewModels;
-using Dev.Business.Models;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.Extensions.DependencyInjection;
 
-namespace DevIO.Api.Configuration
-{
-    public static IServiceCollection WebApiConfig(this IServiceCollection services)
-    {
-        services.AddAutoMapper(typeof(Startup));
-        return services;
-    }
+// namespace DevIO.Api.Configuration
+// {
+//     public static class ApiConfig
+//     {
+//         public static IServiceCollection WebApiConfig(this IServiceCollection services)
+//         {
+//             builder.Services.AddControllers()
+//             .AddJsonOptions(options =>
+//             {
+//             options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+//             });
 
-    
-    public static IApplicationBuilder UseMvcConfiguration(this IApplicationBuilder app)
-        {
-            return app;
-        }
-    
-}
+//             builder.Services.AddCors(options =>
+//             {
+//                 options.AddPolicy("AllowAngular",
+//                     policy => policy
+//                         .WithOrigins("http://localhost:4200")
+//                         .AllowAnyHeader()
+//                         .AllowAnyMethod());
+//             });
+
+//             builder.Services.Configure<ApiBehaviorOptions>(options =>
+//             {
+//                 options.SuppressModelStateInvalidFilter = true;
+//             });
+
+
+//         }
+
+//         public static IApplicationBuilder UseMvcConfiguration(this IApplicationBuilder app)
+//         {
+            
+//             app.UseHttpsRedirection();
+
+
+//             app.UseCors("AllowAngular");
+
+//             app.MapControllers();
+
+//             return app;
+//         }
+//     }
+// }
